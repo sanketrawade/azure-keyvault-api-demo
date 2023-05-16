35c6041cc9e550cec0bfeb85e012967d73458cc6
sanketrawade <sanketrawade11@gmail.com>
2023-05-16 17:48:02 +0530

    first commit

---
+++ Controllers/SecretsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace keyvault_demo_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretsController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        //get secret from keyvault
+        [HttpGet]
+        public List<string> Get()
+        {
+            List<string> result = new List<string>() {
+                _configuration["SECRETNAME"]
+            };
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Azure.Identity;
+
+namespace keyvault_demo_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Configuration.
+                AddAzureKeyVault(new Uri
+                ($"https://{builder.Configuration["Azure:keyvaultName"]}.vault.azure.net/"),
+                new DefaultAzureCredential());
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
